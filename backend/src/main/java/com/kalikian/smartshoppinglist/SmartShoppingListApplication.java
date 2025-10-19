d667025146fcd86dbd62bd67fb4c8943f8b14328
Agop Kalikian <hagopk82@outlook.com>
2025-10-19 12:58:12 +0200

    chore: initialize backend project (Spring Boot setup)

---
+++ backend/src/main/java/com/kalikian/smartshoppinglist/SmartShoppingListApplication.java
@@ -0,0 +1,16 @@
+package com.kalikian.smartshoppinglist;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShoppingListApplication {
+        public static void main(String[] args) {
+            // Print a welcome message
+            System.out.println("Hello and welcome!");
+
+            // Print numbers from 1 to 5
+            for (int i = 1; i <= 5; i++) {
+                System.out.println("i = " + i);
+            }
+    }
+}
